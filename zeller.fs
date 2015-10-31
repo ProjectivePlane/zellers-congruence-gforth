0df42624261ded7f3c6a3399f1b1ce943b57337d
ProjectivePlane <derek.bell.ie@gmail.com>
2015-10-31 19:47:30 +0000

    Committing calculation words, corrected constants.

--- zeller.fs
+++ zeller.fs
@@ -1,10 +1,12 @@
-0 constant sunday
-1 constant monday
-2 constant tuesday
-3 constant wednesday
-4 constant thursday
-5 constant friday
-6 constant saturday
+0 constant saturday
+1 constant sunday
+2 constant monday
+3 constant tuesday
+4 constant wednesday
+5 constant thursday
+6 constant friday
+
+
 
 3 constant march
 4 constant april
@@ -25,3 +27,50 @@
 	12 + >r
 	1- r>
     then r> ;
+
+: calculation-intermediates
+    rot rot
+    swap
+    dup 100 mod swap
+    100 / ;
+
+: month-term
+    1+ 13 * 5 / ;
+
+: year-term
+    dup 4 / + ;
+
+: gregorian-century-term
+    dup 4 / swap
+    5 * + ;
+
+: julian-century-term
+    5 swap 6 * - ;
+
+: gregorian-sum ( nq nm nK nJ -- nSum)
+    gregorian-century-term
+     >r
+     year-term
+     >r
+     month-term
+     +
+     r>
+     +
+     r>
+     + ;
+
+: gregorian-day-of-week ( ny nm nd - ndow)
+    modified-date
+    calculation-intermediates
+    gregorian-sum 7 mod ;
+
+: julian-sum ( nq nm nK nJ -- nSum)
+    julian-century-term >r
+    year-term >r
+    month-term +
+    r> + r> + ;
+
+: julian-day-of-week ( ny nm nd - ndow)
+    modified-date
+    calculation-intermediates
+    julian-sum 7 mod ;
